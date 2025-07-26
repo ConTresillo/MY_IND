9d2dc8777c692a9bc01327379fa77999b49e08de
ConTresillo <tharoonsays@gmail.com>
2025-07-26 17:11:19 +0530

    Added backend, cloud folders and readme

---
+++ backend/data_processor.py
@@ -0,0 +1,10 @@
+# data_processor.py.py
+
+# Header
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ backend/scraper.py
@@ -0,0 +1,10 @@
+# scraper.py.py
+
+# Header
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
